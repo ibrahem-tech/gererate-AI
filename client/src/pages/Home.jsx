dfb34d0acaee84164ac1ba492a6309bf08514cf3
Ibrahim <ibrahim@ibrahims-mbp.home>
2023-12-24 23:44:30 -0500

    creating the tabs on customizer page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,54 +5,54 @@ import { useSnapshot } from "valtio";
 import state from "../store";
 
 import {
-	headContainerAnimation,
-	headContentAnimation,
-	headTextAnimation,
-	slideAnimation,
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
 } from "../config/motion";
 import { CustomButton } from "../components";
 
 const Home = () => {
-	const snap = useSnapshot(state);
-	return (
-		<AnimatePresence>
-			{snap.intro && (
-				<motion.section className="home" {...slideAnimation("left")}>
-					<motion.header {...slideAnimation("down")}>
-						<img
-							src="./threejs.png"
-							alt="logo"
-							className="w-8 h-8 object-contain"
-						/>
-					</motion.header>
-					<motion.div className="home-content" {...headContainerAnimation}>
-						<motion.div {...headTextAnimation}>
-							<h1 className="head-text">
-								LET'S <br className="xl-block hidden" /> MAKE IT A THING
-							</h1>
-						</motion.div>
-						<motion.div
-							{...headContentAnimation}
-							className="flex flex-col gap-5"
-						>
-							<p className="max-w-md font-normal text-gray-600">
-								Design your one-of-a-kind and premium shirt using our innovative
-								3D customization tool.
-								<strong> Set your creativity free </strong>and establish a style
-								that is uniquely yours.
-							</p>
-							<CustomButton
-								type="filled"
-								title="Customize It"
-								handleClick={() => (state.intro = false)}
-								customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-							/>
-						</motion.div>
-					</motion.div>
-				</motion.section>
-			)}
-		</AnimatePresence>
-	);
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl-block hidden" /> MAKE IT A THING
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600">
+                Design your one-of-a-kind and premium shirt using our innovative
+                3D customization tool.
+                <strong> Set your creativity free </strong>and establish a style
+                that is uniquely yours.
+              </p>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Home;
