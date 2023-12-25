413fa10aede15e6b55662ef7fc4df95844ad5d63
Ibrahim <ibrahim@ibrahims-mbp.home>
2023-12-24 23:32:07 -0500

    setting up the canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
 
-const Canvas = () => {
-	return <div>Canvas</div>;
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CAmeraRig";
+
+const CanvasModel = () => {
+  return (
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 30 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
