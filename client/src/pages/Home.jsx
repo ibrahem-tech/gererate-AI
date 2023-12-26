65d1ba647ab5891ab8b69c63b09fc2fdce496509
Ibrahim <ibrahim@ibrahims-mbp.home>
2023-12-25 21:02:09 -0500

    deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,14 +17,6 @@ const Home = () => {
     <AnimatePresence>
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
-          {/* <motion.header {...slideAnimation("down")}>
-            <img
-              src="./threejs.png"
-              alt="logo"
-              className="w-8 h-8 object-contain"
-            />
-          </motion.header> */}
-
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
@@ -36,9 +28,9 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-slate-300 text-base">
-                Create your unique and exclusive shirt with our brand-new 3D
-                customization tool. <strong>Unleash your imagination</strong>{" "}
-                and define your own style.
+                Design your one-of-a-kind shirt using our state-of-the-art 3D
+                customization tool. <strong> Set your creativity free </strong>{" "}
+                and shape your distinctive style
               </p>
 
               <CustomButton
