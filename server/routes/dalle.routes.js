3c28a60952c1a66ed101479b1b03e08fba552e84
Ibrahim <ibrahim@ibrahims-mbp.home>
2023-12-25 20:44:14 -0500

    finishing up the app

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,28 +7,28 @@ dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
-  res.status(200).json({
-    message: "hello from ibra 2.0",
-  });
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
-    const image = response.data.data[0].b64_json;
+    const image = response.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
